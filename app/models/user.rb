55278bdb837b061156b67b281dba2eab4e2ebef5
Khan1thu <bienew311@gmail.com>
2022-09-11 15:40:04 +0630

    User validation done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
 
+    validates :username, uniqueness: true, length: { minimum: 4, maximum: 12}, presence: true
+    validates :email, uniqueness: true, presence: true
+    validates :password, length: {minimum: 6, maximum: 16}, presence: true
     has_many :posts
 end
