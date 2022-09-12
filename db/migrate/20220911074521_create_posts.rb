19cb0ab3222995f6eeea668de9f1160223256971
Khan1thu <bienew311@gmail.com>
2022-09-12 16:46:16 +0630

    Comments table created and migrated

--- db/migrate/20220911074521_create_posts.rb
+++ db/migrate/20220911074521_create_posts.rb
@@ -4,7 +4,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.string :title
       t.text :body
       t.integer :user_id
-      t.belongs_to :user
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
